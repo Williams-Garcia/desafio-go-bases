d13590ef30e6030ca48fbb02efffec5028f72f8c
Williams-Garcia <williams.garcia@mercadolibre.com.mx>
2022-12-30 09:17:28 -0600

    Complete unit test

--- internal/tickets/tickets_test.go
+++ internal/tickets/tickets_test.go
@@ -1,5 +1,87 @@
 package tickets
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestGetTotalTickets(t *testing.T) {}
+func TestGetTotalTicketsBrazil(t *testing.T) {
+	//Arrange
+	data := readFile("/Users/williamgarci/Documents/bootcamp/desafio-go-bases/tickets.csv")
+	ticketList := convertToJson(data)
+	brazilPeople := 45
+
+	//Act
+	result, _ := GetTotalTickets("Brazil", ticketList)
+
+	//Assert
+	if result != brazilPeople {
+		t.Errorf("Prueba fallo")
+	}
+}
+
+func TestGetTotalTicketsMexico(t *testing.T) {
+	//Arrange
+	data := readFile("/Users/williamgarci/Documents/bootcamp/desafio-go-bases/tickets.csv")
+	ticketList := convertToJson(data)
+	mexicoPeople := 11
+
+	//Act
+	result, _ := GetTotalTickets("Mexico", ticketList)
+
+	//Assert
+	if result != mexicoPeople {
+		t.Errorf("Prueba fallo")
+	}
+}
+
+func TestGetTotalTicketsFailed(t *testing.T) {
+	//Arrange
+	data := readFile("/Users/williamgarci/Documents/bootcamp/desafio-go-bases/tickets.csv")
+	ticketList := convertToJson(data)
+
+	//Act
+	_, err := GetTotalTickets("", ticketList)
+
+	//Assert
+	if err != nil {
+		t.Log("Prueba Erronea exitosa")
+	}
+}
+
+func TestAverageDestinationMexxico(t *testing.T) {
+	// Arrange
+	peopleInMexico := 11
+	totalPeople := 999
+	avg := 0.011011011011011011
+
+	// Act
+	result, _ := AverageDestination(peopleInMexico, totalPeople)
+
+	// Assert
+	if result != avg {
+		t.Errorf("Prueba fallo")
+	}
+}
+
+func TestGetPeriods(t *testing.T) {
+	// Arrange
+	data := readFile("/Users/williamgarci/Documents/bootcamp/desafio-go-bases/tickets.csv")
+	ticketList := convertToJson(data)
+
+	// Act
+	midnight, morning, evening, night := getPeriod(ticketList)
+
+	// Assert
+	if midnight != 304 {
+		t.Errorf("Midnight fallo")
+	}
+	if morning != 256 {
+		t.Errorf("Morning fallo")
+	}
+	if evening != 288 {
+		t.Errorf("Evening fallo")
+	}
+	if night != 151 {
+		t.Errorf("Night fallo")
+	}
+}
--- main.go
+++ main.go
@@ -4,10 +4,6 @@ import (
 	"github.com/bootcamp-go/desafio-go-bases/internal/tickets"
 )
 
-var (
-	Kitchen = "3:04"
-)
-
 // Una aerolínea pequeña tiene un sistema de reservas de pasajes a diferentes países, este retorna un archivo con la información de
 // los pasajes sacados en las últimas 24 horas.
 // La aerolínea necesita un programa para extraer información de las ventas del día y así, analizar las tendencias de compra.
@@ -15,12 +11,5 @@ var (
 // por: id, nombre, email, país de destino, hora del vuelo y precio.
 
 func main() {
-	// total, err := tickets.GetTotalTickets("Brazil")
 	tickets.Init()
-	// now, err := time.Parse(Kitchen, "15:45")
-	// if err != nil {
-	// 	println(err)
-	// }
-	// // the time and date in this string is intentional, as we will discuss soon
-	// fmt.Println(now)
 }
